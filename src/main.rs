bb951bcdc5eb976644f51bfe0b7c87560fbcdac0
khannedy <echo.khannedy@gmail.com>
2024-12-03 15:47:38 +0700

    Add logging tests and separate log configuration.
    This commit introduces a new test module, `tests2`, with additional logging tests to verify log functionality. It also updates the log4rs configuration to include a separate log file, `tests.log`, for test logs, and configures different log levels for test modules.

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,21 @@ mod tests {
     }
 
 }
+
+#[cfg(test)]
+mod tests2 {
+
+    use log::{debug, error, info, trace, warn};
+
+    #[test]
+    fn test_log(){
+        log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+
+        error!("This is an error");
+        warn!("This is a warning");
+        info!("This is an info");
+        debug!("This is a debug");
+        trace!("This is a trace");
+    }
+
+}
