172d5256ed3245113c68bd52b3b84fee67bbfc8b
khannedy <echo.khannedy@gmail.com>
2024-12-03 15:16:35 +0700

    Add basic logging tests
    Introduce a test module with a function to verify logging at various levels: error, warn, info, debug, and trace. This ensures that the logging library is correctly configured and outputs expected messages.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    use log::{debug, error, info, trace, warn};
+
+    #[test]
+    fn test_log(){
+        error!("This is an error");
+        warn!("This is a warning");
+        info!("This is an info");
+        debug!("This is a debug");
+        trace!("This is a trace");
+    }
+
+}
